e2e1f77caac4233d1b771ad7cb930c4ea567e49e
David Pflug <david@tpflug.com>
2012-01-12 16:56:48 -0500

    1-dpflug: Generalizing a bit

--- lisp/dpflug-0001.lisp
+++ lisp/dpflug-0001.lisp
@@ -1,7 +1,8 @@
-(print
-  (eval
-    (cons '+ (loop for x from 1 to 999
-                   if (or
-                        (= 0 (rem x 5))
-                        (= 0 (rem x 3))
-                      ) collect x))))
+(defun gaussian-addition (upper-limit)
+  (loop for x from 1 to upper-limit
+        if (or
+             (= 0 (rem x 5))
+             (= 0 (rem x 3))
+           ) sum x))
+
+(print (gaussian-addition 999))
