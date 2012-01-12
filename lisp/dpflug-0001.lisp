8a092fa1f1e659b2c2c7f7e12a90342fdca45d13
David Pflug <david@tpflug.com>
2012-01-12 17:01:22 -0500

    1-dpflug: Not gaussian addition.

--- lisp/dpflug-0001.lisp
+++ lisp/dpflug-0001.lisp
@@ -1,8 +1,8 @@
-(defun gaussian-addition (upper-limit)
+(defun euler1 (upper-limit)
   (loop for x from 1 to upper-limit
         if (or
              (= 0 (rem x 5))
              (= 0 (rem x 3))
            ) sum x))
 
-(print (gaussian-addition 999))
+(print (euler1 999))
