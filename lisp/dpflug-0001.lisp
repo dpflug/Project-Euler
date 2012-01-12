b43369caf29dfd231682da59edbeec6bd4830cf7
David Pflug <david@tpflug.com>
2012-01-12 16:35:54 -0500

    1-dpflug: Indentation

--- lisp/dpflug-0001.lisp
+++ lisp/dpflug-0001.lisp
@@ -1,1 +1,7 @@
-(print (eval (cons '+ (loop for x from 1 to 999 if (or (= 0 (rem x 5)) (= 0 (rem x 3))) collect x))))
+(print
+  (eval
+    (cons '+ (loop for x from 1 to 999
+                   if (or
+                        (= 0 (rem x 5))
+                        (= 0 (rem x 3))
+                      ) collect x))))
