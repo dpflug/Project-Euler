18c47f36ff43ca3fd6a7cf214b9a0b19b2919281
David Pflug <david@tpflug.com>
2012-01-13 13:50:35 -0500

    1-dpflug: Added a recursive version, just because

--- lisp/dpflug-0001.lisp
+++ lisp/dpflug-0001.lisp
@@ -1,8 +1,20 @@
 (defun euler1 (upper-limit)
+  "Simple loop."
   (loop for x from 1 to upper-limit
-        if (or
+        if
+            (or
              (= 0 (rem x 5))
-             (= 0 (rem x 3))
-           ) sum x))
+             (= 0 (rem x 3))) sum x))
+
+(defun euler1-recursive (x)
+  "Recursive version"
+  (cond
+    ((or
+      (= 0 (rem x 5))
+      (= 0 (rem x 3)))
+       (+ x (euler1-recursive (1- x))))
+    ((< x 3 ) 0)
+    (t (+ 0 (euler1-recursive (1- x))))))
 
 (print (euler1 999))
+(print (euler1-recursive 999))
