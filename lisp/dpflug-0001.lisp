3d6a5c9ed835823470c4fcf16ad8950135698611
David Pflug <david@tpflug.com>
2012-01-13 15:15:25 -0500

    1-dpflug: Now with tail recursion!

--- lisp/dpflug-0001.lisp
+++ lisp/dpflug-0001.lisp
@@ -16,5 +16,16 @@
     ((< x 3 ) 0)
     (t (+ 0 (euler1-recursive (1- x))))))
 
+(defun euler1-tail (x &optional (sum 0))
+  "Tail-recursive version"
+  (cond
+    ((< x 3) sum)
+    ((or
+       (= 0 (rem x 5))
+       (= 0 (rem x 3)))
+         (euler1-tail (1- x) (+ x sum)))
+    (t (euler1-tail (1- x) sum))))
+
 (print (euler1 999))
 (print (euler1-recursive 999))
+(print (euler1-tail 999))
